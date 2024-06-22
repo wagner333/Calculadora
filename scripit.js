f30c8302e4c8b2af7b4ce208e47daec9412c2002
Wagner da Silva Camargo Filho <144560716+wagner333@users.noreply.github.com>
2024-06-22 11:04:38 -0300

    Add files via upload

---
+++ scripit.js
@@ -0,0 +1,9 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    let expression = document.getElementById('display').value;
+    let result = eval(expression); 
+    document.getElementById('display').value = result;
+}
